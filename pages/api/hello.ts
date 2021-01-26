fc7d4e658998ff40ab0e4dc75a5143d1c0696a0c
bosskab123 <phatarapran7524@gmail.com>
2021-01-27 02:53:42 +0700

    install some dependencies

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import mysql from 'mysql'
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'onestopdb'
+})
+
+connection.connect();
 
 export default (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ text: 'Hello' })
 }
 
+connection.end(function (err) {
+  
+});
+
+connection.destroy();
