9b3644f7a2e4d3be2ded13216b91a453158216b7
Phatarapran Saraluck <phatarapran7524@gmail.com>
2021-01-28 03:16:42 +0700

    Revert "Test"
    This reverts commit 521bb8a95f5fa8a600bfd235b0af6b64e75d1de1.

--- db-test.js
+++
@@ -1,8 +0,0 @@
-const sqlist = require('sqlite');
-
-async function setup(){
-    const db = await sqlite.open('./mydb.sqlite');
-    await db.migrate({force: 'last'})
-}
-
-setup();
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,4 +3,3 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ text: 'Hello' })
 }
-
