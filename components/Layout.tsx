fdbf1e6c727e91a05741cc6f79c54daaf50466e4
Phatarapran Saraluck <phatarapran7524@gmail.com>
2021-01-28 16:41:53 +0700

    Add bullettin page
    basic algorithm to select major, minor

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,18 +1,44 @@
-import React, { ReactElement } from 'react'
-import Hamburger from 'hamburger-react'
+import React, { ReactElement, useState } from "react";
+import Hamburger from "hamburger-react";
+import Link from "next/link";
 
 interface Props {
   children: ReactElement
 }
 
 export default function Layout({children}: Props): ReactElement {
+  const [isLeftPaneOpen, setLeftPaneOpen] = useState(false);
   return (
     <div className="container w-full max-w-full">
-      <div className="w-full flex-row inline-flex items-center bg-gray-300 space-x-3">
-        <div><Hamburger/></div>
+       {/* Left Pane*/}
+       <div 
+        className = {`
+          fixed bg-gray-400 xs:w-1/2 sm:w-1/6 w-1/3 h-full 
+          transition-transform transform p-16 
+          ${isLeftPaneOpen ? "" : "-translate-x-full"}
+        `}
+      >
+        <ul>
+          <li>
+            <Link href="/bulletin">
+              <a> Bulletin </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/my-courses">
+              <a> My Courses </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      {/* Nav bar */}
+      <div className="w-full flex-row inline-flex items-center bg-gray-300 space-x-3 text-xl font-sans font-black">
+        <Hamburger toggled={isLeftPaneOpen} toggle={setLeftPaneOpen} />
         <span> One Stop Academic System </span>
       </div>
-      {children}
+      <div>
+        {children}
+      </div>
     </div>
   )
 }
--- pages/bulletin.tsx
+++ pages/bulletin.tsx
@@ -1,10 +1,96 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
+import { useState } from 'react';
+
+// fake departments
+
+const departments = [{name: "Computer Science"},{name: "Electrical Engineering"},{name: "Industrial Engineering"}];
 
 export default function Bulletin() {
+  
+  const [advanceMajor, setAdvanceMajor] = useState("");
+  const [major, setMajor] = useState("");
+  const [minor, setMinor] = useState("");
+  const [doubleMajor, setDoubleMajor] = useState("");
+
+  function selectMinor(department){
+    if(doubleMajor != ""){
+      setMajor("");
+      setDoubleMajor("");
+    }
+    else if(advanceMajor != ""){
+      setAdvanceMajor("");
+    }
+    setMinor(department.name);
+  }
+
+  function selectMajor(department){
+    if(major != "" && minor == ""){
+      setDoubleMajor(department.name);
+    }
+    else if(major != "" && minor != ""){
+      setMinor("");
+      setMajor(department.name);
+    }
+    else if(doubleMajor != ""){
+      setDoubleMajor("");
+      setMajor(department.name);
+    }
+    else if(advanceMajor != ""){
+      setAdvanceMajor("");
+      setMajor(department.name);
+    }
+    else{
+      setMajor(department.name);
+    }
+
+  }
+
+  function selectAdvanceMajor(department){
+    if(minor != ""){
+      setMinor("");
+    }
+    if(major != ""){
+      setMajor("");
+    }
+    if(doubleMajor != ""){
+      setDoubleMajor("");
+      setMajor("");
+    }
+    setAdvanceMajor(department.name);
+  }
   return (
     <Layout>
-      <> Bulletin </>
+      <div className = "container mx-auto h-screen border-2 px-4 border-black"> 
+        <p className = "font-bold text-3xl font-sans mt-9 mb-9"> Bulletin </p>
+        <div className = "font-medium">
+          <p> To show the graduation criteria</p>
+          <p> Select the red star on major department and the blue star on minor department</p>
+          <p> Select only the green star on advanced major department</p>
+          <p> Select the red star on major departments</p>
+        </div>
+        <div className="flex border-2">
+          <div className = "container text-2xl mt-4 mb-4 mr-3">
+            { departments.map( department => 
+                <div className="bg-gray-100 w-full border-2 border-gray-200 p-2 mt-2 mb-2 hover:bg-gray-200 flex"> 
+                  <div className="w-1/2 text-center pt-3"> {department.name} </div>
+                  <div className=" text-xl text-center flex">
+                    <button className="rounded-md bg-red-100 ml-5 mr-5 font-medium pl-2 pr-2 hover:bg-red-200" onClick={()=>{selectMinor(department)}}>Minor</button>
+                    <button className="rounded-md bg-blue-100 ml-5 mr-5 font-medium pl-2 pr-2 hover:bg-blue-200" onClick={()=>{selectMajor(department)}}>Major</button>
+                    <button className="rounded-md bg-green-100 ml-5 mr-5 font-medium pl-2 pr-2 hover:bg-green-200" onClick={()=>{selectAdvanceMajor(department)}}>Advanced Major</button>
+                  </div>
+                </div>
+            )}
+          </div>
+          <div className ="container h-full bg-gray-100 mt-4">
+              <p>Major: {major}</p>
+              <p>Minor: {minor}</p>
+              <p>Advance Major: {advanceMajor}</p>
+              <p>Double Major: {doubleMajor}</p>
+          </div>
+        </div>
+
+      </div>
     </Layout>
   )
 }
